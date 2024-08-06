a65c383ddafb5b1bf7464dc8bd829a90561aa451
ularbekergeshov <ulk@mail.com>
2024-08-05 20:08:18 -0500

    adding files

--- src/gitClass.java
+++ src/gitClass.java
@@ -1,7 +1,12 @@
 public class gitClass {
 
     public static void main(String[] args) {
+
+
         System.out.println("New branch");
         System.out.println("Main");
+        System.out.println("Project Git");
+
+
     }
 }
